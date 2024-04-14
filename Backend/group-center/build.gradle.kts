82fadeec87c13b8b1154a36b61dde7f1a3800d9b
Haomin Kong <a645162@qq.com>
2024-04-14 17:22:44 +0800

    添加数据库

--- Backend/group-center/build.gradle.kts
+++ Backend/group-center/build.gradle.kts
@@ -19,10 +19,31 @@ repositories {
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
+
+    // Spring Boot
+    implementation("org.springframework.boot:spring-boot-starter-web")
+
+    // Database
+    // https://mvnrepository.com/artifact/com.alibaba/druid-spring-boot-starter
+    implementation("com.alibaba:druid-spring-boot-starter:1.2.22")
+
+    implementation("org.mybatis.spring.boot:mybatis-spring-boot-starter:3.0.3")
+    implementation("com.baomidou:mybatis-plus-spring-boot3-starter:3.5.6")
+
+    // Database Driver
+    // https://mvnrepository.com/artifact/com.mysql/mysql-connector-j
+    runtimeOnly("com.mysql:mysql-connector-j:8.3.0")
+    // https://mvnrepository.com/artifact/com.oceanbase/oceanbase-client
+    implementation("com.oceanbase:oceanbase-client:2.4.9")
+
+    // Crypto
+//    implementation("com.tencent.kona:kona-crypto")
+//    implementation("com.tencent.kona:kona-provider")
+
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.mybatis.spring.boot:mybatis-spring-boot-starter-test:3.0.3")
 }
 
 tasks.withType<KotlinCompile> {
---
+++ README.md
@@ -0,0 +1,31 @@
+# Group Center
+
+课题组消息中心
+
+## 快速开始
+
+## 框架及工具
+
+### 语言及构建工具
+
+- Kotlin
+- Gradle(Kotlin)
+
+### 框架
+
+- Spring Boot
+
+### 数据库
+
+- MyBatis
+- MyBatis Plus
+- MySQL
+- OceanBase
+
+### 加密
+
+- Tencent KONA(国密SM算法)
+
+## License
+
+[GPL-3.0](./LICENSE)
