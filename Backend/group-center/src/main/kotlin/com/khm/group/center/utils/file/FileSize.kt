fda7f5cc999460621edf33e742dde89e655dcb82
Haomin Kong <a645162@qq.com>
2024-06-07 12:49:33 +0800

    Fix bug

--- Backend/group-center/src/main/kotlin/com/khm/group/center/utils/file/FileSize.kt
+++ Backend/group-center/src/main/kotlin/com/khm/group/center/utils/file/FileSize.kt
@@ -2,7 +2,7 @@ package com.khm.group.center.utils.file
 
 class FileSize {
     companion object {
-        fun fixText(originalText: String) {
+        fun fixText(originalText: String): String {
             var newText = originalText
 
             while (newText.contains("GiB")) {
@@ -12,6 +12,12 @@ class FileSize {
             while (newText.contains("MiB")) {
                 newText = newText.replace("Mib", "MB")
             }
+
+            while (newText.contains("KiB")) {
+                newText = newText.replace("KiB", "KB")
+            }
+
+            return newText
         }
     }
 }
