ab6ea559b5be84ea46b8451c376b4fe552ef90da
Haomin Kong <a645162@qq.com>
2025-10-08 21:56:07 +0800

    feat: 更新 MachineHeartbeat 类，移除序列化注解并将字段改为可变类型，添加默认值以支持 JSON 反序列化

--- src/main/kotlin/com/khm/group/center/datatype/receive/heartbeat/MachineHeartbeat.kt
+++ src/main/kotlin/com/khm/group/center/datatype/receive/heartbeat/MachineHeartbeat.kt
@@ -1,12 +1,9 @@
 package com.khm.group.center.datatype.receive.heartbeat
 
-import kotlinx.serialization.Serializable
-
 /**
  * 机器心跳请求数据
  */
-@Serializable
 data class MachineHeartbeat(
-    val timestamp: Long,  // 客户端时间戳
-    val serverNameEng: String  // 服务器英文名称
+    var timestamp: Long = 0,  // 客户端时间戳
+    var serverNameEng: String = ""  // 服务器英文名称
 )
