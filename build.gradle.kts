6ece94d7adec60b795310e33b8a2084b7d9450da
Haomin Kong <a645162@qq.com>
2025-10-13 23:04:48 +0800

    feat: 添加通过taskId精确匹配任务的功能，支持获取特定GPU任务；更新相关服务逻辑

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,7 +27,7 @@ val myBatisVersion = "3.0.5"
 val fastjsonVersion = "2.0.59"
 
 group = "com.khm.group"
-version = "1.5.2-SNAPSHOT"
+version = "1.5.3-SNAPSHOT"
 
 java {
     sourceCompatibility = JavaVersion.VERSION_24
